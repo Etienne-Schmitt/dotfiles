4998d01651a6443426a6a47df50713fcc5cbd6f8
Etienne Schmitt <etienne@schmitt-etienne.fr>
2023-02-28 13:15:55 +0100

    Base config

---
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,17 @@
+# Custom command
+alias json-pretty='python3 -m json.tool'
+alias dstan='docker run -itv $PWD:/app --rm ghcr.io/phpstan/phpstan:latest-php8.2'
+
+# Shortcut
+alias k='kubectl'
+alias kns='kubectl config set-context --current -n'
+alias h='helm'
+
+alias tt='tree -dL 2'
+alias ttt='tree -dL 3'
+alias tttt='tree -dL 4'
+
+# Replacing shortcut
+alias vim="nvim"
+alias rgrep="rgrep -n"
+
